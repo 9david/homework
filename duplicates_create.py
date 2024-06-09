f8762ccc340e78ea0fa369872a2b96e70e00d814
David <david.arutyunyan98@gmail.com>
2024-06-09 18:37:15 +0300

    homework

---
+++ duplicates_create.py
@@ -0,0 +1,35 @@
+import json
+import csv
+from difflib import SequenceMatcher
+from create_data import save_data_to_json
+
+def similarity(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+def find_duplicates(data, key, threshold=0.8):
+    data_sorted = sorted(data, key=lambda x: x[key])
+    duplicates = []
+    visited = [False] * len(data_sorted)
+
+    for i in range(len(data_sorted) - 1):
+        if visited[i]:
+            continue
+        group = [data_sorted[i]]
+        for j in range(i + 1, len(data_sorted)):
+            if similarity(data_sorted[i][key], data_sorted[j][key]) >= threshold:
+                group.append(data_sorted[j])
+                visited[j] = True
+        if len(group) > 1:
+            duplicates.append({"Duplicate": group})
+
+    return duplicates
+
+with open('tv_data.js', 'r', encoding='utf-8') as jsonfile:
+    data = json.load(jsonfile)
+
+# Поиск дубликатов
+duplicates = find_duplicates(data, key='Название')
+
+# Сохранение дубликатов в формате JSON
+save_data_to_json(duplicates, 'duplicates_data.js', duplicate=True)
+
---
+++ duplicates_create_mrjob.py
@@ -0,0 +1,38 @@
+import json
+from difflib import SequenceMatcher
+from mrjob.job import MRJob
+from create_data import save_data_to_json
+
+
+def similarity(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+class FindDuplicatesJob(MRJob):
+
+    def mapper(self, _, line):
+        data = json.loads(line)
+        yield data['Название'], data
+
+    def reducer(self, key, values):
+        data_list = list(values)
+        if len(data_list) > 1:
+            yield None, {"Duplicate": data_list}
+
+if __name__ == '__main__':
+    # Создание экземпляра MRJob
+    job = FindDuplicatesJob()
+
+    # Запуск MRJob
+    with job.make_runner() as runner:
+        # Чтение входного файла с данными
+        with open('tv_data.js', 'r', encoding='utf-8') as jsonfile:
+            # Запуск MapReduce задачи
+            runner.run()
+
+            # Обработка результатов
+            duplicates = []
+            for _, output in job.parse_output(runner.cat_output()):
+                duplicates.append(output)
+
+    # Сохранение дубликатов в формате JSON
+    save_data_to_json(duplicates, 'duplicates_data_mrjob.js', duplicate=True)
